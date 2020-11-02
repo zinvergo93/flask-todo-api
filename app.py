eb2646be206d3dbbc99b1c8661d0732df43a6376
zinvergo93 <zachary.coding@gmail.com>
2020-11-02 11:15:15 -0600

    add app.sqlite

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Todo(db.Model):
+    __tablename = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "done")
+
+todo_schema = TodoSchema()
+todos_schea = TodoSchema(many=True)
 
 @app.route("/")
 def hello():
